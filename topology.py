d422ba5c9f9be2df00a68604b5eef41c1639141d
tjadczak <tomasz.jadczak1996@gmail.com>
2020-03-22 15:44:09 +0100

    Update

--- topology.py
+++ topology.py
@@ -1,34 +1,27 @@
-"""
-Custom topology example
-Two directly connected switches plus a host for each switch:
-    host ---switch ---switch ---host
-Adding the 'topos' dict with a key/value pair to generate our newly defined
-topology enables one to pass in '--topo=mytopo' from the command line.
-"""
-
 from mininet.topo import Topo
-class MyTopo( Topo ):
-    def __init__( self ):
+
+
+class MyTopo(Topo):
+    def __init__(self):
         # Initialize topology
-        Topo.__init__( self )
-        
+        Topo.__init__(self)
+
         # Add hosts and switches
-        leftHost = self.addHost( 'h1' )
-        rightHost = self.addHost( 'h5' )
-        SwitchA = self.addSwitch( 's1' )
-        SwitchB = self.addSwitch( 's2' )
-        SwitchC = self.addSwitch( 's3' )
-        SwitchD = self.addSwitch( 's4' )
-        SwitchE = self.addSwitch( 's5' )
-        
+        host_1 = self.addHost('h1')
+        host_2 = self.addHost('h2')
+        host_3 = self.addHost('h3')
+        server_11 = self.addHost('h11')
+        server_12 = self.addHost('h12')
+        server_13 = self.addHost('h13')
+        switch = self.addSwitch('s1')
+
         # Add Links
-        self.addLink( leftHost, SwitchA, bw=30 )
-        self.addLink( SwitchA, SwitchB, bw=10 )
-        self.addLink( SwitchA, SwitchC, bw=10 )
-        self.addLink( SwitchA, SwitchD, bw=10 )
-        self.addLink( SwitchB, SwitchE, bw=10 )
-        self.addLink( SwitchC, SwitchE, bw=10 )
-        self.addLink( SwitchD, SwitchE, bw=10 )
-        self.addLink( SwitchE, rightHost, bw=30 )
-        
-topos = { 'mytopo': ( lambda: MyTopo() ) }
+        self.addLink(switch, host_1, bw=10)
+        self.addLink(switch, host_2, bw=10)
+        self.addLink(switch, host_3, bw=10)
+        self.addLink(switch, server_11, bw=10)
+        self.addLink(switch, server_12, bw=10)
+        self.addLink(switch, server_13, bw=10)
+
+
+topos = {'mytopo': (lambda: MyTopo())}
