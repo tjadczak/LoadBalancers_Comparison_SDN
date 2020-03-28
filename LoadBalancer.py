16b77d442f78b47fb7e53552b1fa888d75131580
tjadczak <tomasz.jadczak1996@gmail.com>
2020-03-28 16:16:39 +0100

    Update

--- LoadBalancer.py
+++ LoadBalancer.py
@@ -86,14 +86,13 @@ class SimpleLoadBalancer(app_manager.RyuApp):
     @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
     def _flow_stats_reply_handler(self, ev):
         body = ev.msg.body
-        self.logger.info('        datapath ' ' in-port ' 'out-port  packets    bytes')
-        self.logger.info('---------------- ' '-------- ' '-------- -------- --------')
+        self.logger.info('        datapath ' ' in-port ' 'packets    bytes')
+        self.logger.info('---------------- ' '-------- ' '--------- --------')
         for stat in sorted([flow for flow in body if flow.priority == 1],
                            key=lambda flow: (flow.match['in_port'])):
             self.logger.info('%016x %8x %17s %8x %8d %8d',
                              ev.msg.datapath.id,
                              stat.match['in_port'],
-                             stat.instructions[0].actions[0].port,
                              stat.packet_count, stat.byte_count)
 
     @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
