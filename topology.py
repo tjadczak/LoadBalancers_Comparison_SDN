0a26fc9bfeecf1d37391e8eece4af0c659d75d0f
Tomasz Jadczak <tomasz.jadczak1996@gmail.com>
2020-03-28 09:13:19 -0700

    update

--- topology.py
+++ topology.py
@@ -1,30 +1,44 @@
-from mininet.topo import Topo
+#!/usr/bin/python
+from mininet.cli import CLI
+from mininet.net import Mininet
+from mininet.log import setLogLevel
+from mininet.node import RemoteController
+from mininet.link import TCLink
 
 
-class MyTopo(Topo):
-    def __init__(self):
-        # Initialize topology
-        Topo.__init__(self)
-
-        # Add hosts and switches
-        host_1 = self.addHost('h1')
-        host_2 = self.addHost('h2')
-        host_3 = self.addHost('h3')
-        host_4 = self.addHost('h4')
-        server_1 = self.addHost('h5')
-        server_2 = self.addHost('h6')
-        switch = self.addSwitch('s1')
-
-        # Add Links
-        self.addLink(switch, host_1, bw=10)
-        self.addLink(switch, host_2, bw=10)
-        self.addLink(switch, host_3, bw=10)
-        self.addLink(switch, host_4, bw=10)
-        self.addLink(switch, server_1, bw=10)
-        self.addLink(switch, server_2, bw=10)
-
-        # server_1.sendCmd("python -m SimpleHTTPServer 80 >& /tmp/http.log &")
-        # server_2.sendCmd("python -m SimpleHTTPServer 80 >& /tmp/http.log &")
+REMOTE_CONTROLLER_IP = "127.0.0.1"
 
 
-topos = {'mytopo': (lambda: MyTopo())}
+def main():
+    setLogLevel('info')
+
+    net = Mininet(controller=None, link=TCLink, autoSetMacs=True)
+    net.addController("c0", controller=RemoteController,
+                      ip=REMOTE_CONTROLLER_IP, port=6633)
+    s1 = net.addSwitch('s1')
+
+    host_1 = net.addHost('h1')
+    host_2 = net.addHost('h2')
+    host_3 = net.addHost('h3')
+    host_4 = net.addHost('h4')
+    server_1 = net.addHost('h5')
+    server_2 = net.addHost('h6')
+    switch = net.addSwitch('s1')
+
+    net.addLink(switch, host_1, bw=10)
+    net.addLink(switch, host_2, bw=10)
+    net.addLink(switch, host_3, bw=10)
+    net.addLink(switch, host_4, bw=10)
+    net.addLink(switch, server_1, bw=10)
+    net.addLink(switch, server_2, bw=10)
+
+    net.start()
+    server_1.sendCmd("python -m SimpleHTTPServer 80 >& /tmp/http.log &")
+    server_2.sendCmd("python -m SimpleHTTPServer 80 >& /tmp/http.log &")
+    CLI(net)
+    net.stop()
+
+
+if __name__ == '__main__':
+    main()
+
