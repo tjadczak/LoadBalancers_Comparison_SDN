503ae9e7840d5ba7a2c479b8bed0590c2050e5bd
jadczak <tomasz.jadczak1996@gmail.com>
2020-04-25 20:11:19 +0200

    Buckets

---
+++ BucketLoadBalancer.py
@@ -0,0 +1,260 @@
+from ryu.base import app_manager
+from ryu.controller import ofp_event
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, DEAD_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu.lib.packet.packet import Packet
+from ryu.ofproto import ofproto_v1_3
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import tcp, udp, icmp
+from ryu.lib.packet import ether_types
+from ryu.lib.packet import ipv4
+from ryu.lib.packet import arp
+from operator import attrgetter
+from ryu.lib import hub
+import datetime
+
+""" 
+    TOPOLOGY:
+            h1 --- *            * --- h5 (server)
+                   |            |
+            h2 --- * --- s1 --- * --- h6 (server)
+                   |              
+            h3 --- *            
+                   |
+            h4 --- *
+"""
+
+
+class SimpleLoadBalancer(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+    virtual_ip = "10.0.0.10"  # The virtual server IP
+    # Hosts 5 and 6 are servers.
+    H5_mac = "00:00:00:00:00:05"          # Host 5's mac
+    H5_ip = "10.0.0.5"                    # Host 5's IP
+    H6_mac = "00:00:00:00:00:06"          # Host 6's mac
+    H6_ip = "10.0.0.6"                    # Host 6's IP
+    group_table_id = 50
+    current_server = ""  # Stores the current server's IP
+    ip_to_port = {"10.0.0.1": 1,
+                  "10.0.0.2": 2,
+                  "10.0.0.3": 3,
+                  "10.0.0.4": 4,
+                  "10.0.0.5": 5,
+                  "10.0.0.6": 6}
+    ip_to_mac = {"10.0.0.1": "00:00:00:00:00:01",
+                 "10.0.0.2": "00:00:00:00:00:02",
+                 "10.0.0.3": "00:00:00:00:00:03",
+                 "10.0.0.4": "00:00:00:00:00:04",
+                 "10.0.0.5": "00:00:00:00:00:05",
+                 "10.0.0.6": "00:00:00:00:00:06"}
+    port_to_mac = {1: "00:00:00:00:00:01",
+                   2: "00:00:00:00:00:02",
+                   3: "00:00:00:00:00:03",
+                   4: "00:00:00:00:00:04",
+                   5: "00:00:00:00:00:05",
+                   6: "00:00:00:00:00:06"}
+
+    def __init__(self, *args, **kwargs):
+        super(SimpleLoadBalancer, self).__init__(*args, **kwargs)
+        self.datapaths = {}
+        self.monitor_thread = hub.spawn(self._monitor)
+        self.time_interval = 1
+        # self.next_server = self.H6_ip
+        self.current_server = self.H5_ip
+        self.logger.info("--------------------------------------------------------------")
+        self.logger.info("%s: STARTUP", datetime.datetime.now().strftime('%H:%M:%S.%f'))
+        self.logger.info("--------------------------------------------------------------")
+
+    @set_ev_cls(ofp_event.EventOFPStateChange, [MAIN_DISPATCHER, DEAD_DISPATCHER])
+    def _state_change_handler(self, ev):
+        datapath = ev.datapath
+        if ev.state == MAIN_DISPATCHER:
+            if datapath.id not in self.datapaths:
+                self.logger.info("%s: Register datapath: %s", datetime.datetime.now().strftime('%H:%M:%S.%f'),
+                                 datapath.id)
+                self.datapaths[datapath.id] = datapath
+        elif ev.state == DEAD_DISPATCHER:
+            if datapath.id in self.datapaths:
+                self.logger.info("%s: Unregister datapath: %s", datetime.datetime.now().strftime('%H:%M:%S.%f'),
+                                 datapath.id)
+                del self.datapaths[datapath.id]
+
+    def _monitor(self):
+        while True:
+            #self.logger.info("---------------------------------------------------")
+            #self.logger.info(datetime.datetime.now().strftime("%H:%M:%S"))
+            #self.logger.info("---------------------------------------------------")
+            #for dp in self.datapaths.values():
+            #    self._request_stats(dp)
+            hub.sleep(self.time_interval)
+
+    def _request_stats(self, datapath):
+        self.logger.debug('send stats request: %016x', datapath.id)
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        req = parser.OFPFlowStatsRequest(datapath)
+        datapath.send_msg(req)
+
+        req = parser.OFPPortStatsRequest(datapath, 0, ofproto.OFPP_ANY)
+        datapath.send_msg(req)
+
+    @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
+    def _flow_stats_reply_handler(self, ev):
+        body = ev.msg.body
+        self.logger.info('        datapath ' ' in-port ' 'packets    bytes')
+        self.logger.info('---------------- ' '-------- ' '--------- --------')
+        for stat in sorted([flow for flow in body if flow.priority == 1],
+                           key=lambda flow: (flow.match['in_port'])):
+            self.logger.info('%016x %8x %8d %8d',
+                             ev.msg.datapath.id,
+                             stat.match['in_port'],
+                             stat.packet_count, stat.byte_count)
+
+    @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
+    def _port_stats_reply_handler(self, ev):
+        body = ev.msg.body
+        self.logger.info('datapath         port     '
+                         'rx-pkts  rx-bytes rx-error '
+                         'tx-pkts  tx-bytes tx-error')
+        self.logger.info('---------------- -------- '
+                         '-------- -------- -------- '
+                         '-------- -------- --------')
+        for stat in sorted(body, key=attrgetter('port_no')):
+            self.logger.info('%016x %8x %8d %8d %8d %8d %8d %8d',
+                             ev.msg.datapath.id, stat.port_no,
+                             stat.rx_packets, stat.rx_bytes, stat.rx_errors,
+                             stat.tx_packets, stat.tx_bytes, stat.tx_errors)
+
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # install table-miss flow entry
+
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+        self.send_group_mod(datapath)
+        for host in range(4):
+            actions = [parser.OFPActionGroup(group_id=self.group_table_id)]
+            match = parser.OFPMatch(in_port=host)
+            self.add_flow(datapath, 10, match, actions)
+
+            actions = [parser.OFPActionSetField(ipv4_src=self.virtual_ip),
+                       parser.OFPActionOutput(host)]
+            match = parser.OFPMatch(eth_dst=self.port_to_mac[host])
+            self.add_flow(datapath, 10, match, actions)
+
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def packet_in_handler(self, ev):
+        msg = ev.msg
+        dp = msg.datapath
+        ofp = dp.ofproto
+        ofp_parser = dp.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        etherFrame = pkt.get_protocol(ethernet.ethernet)
+
+        # If the packet is an ARP packet, create new flow table
+        # entries and send an ARP response.
+        if etherFrame.ethertype == ether_types.ETH_TYPE_ARP:
+            self.logger.info("%s: Got Packet In: %s", datetime.datetime.now().strftime('%H:%M:%S.%f'),
+                             "ETH_TYPE_ARP")
+            self.arp_response(dp, pkt, etherFrame, ofp_parser, ofp, in_port)
+            return
+        else:
+            self.logger.warning("Got Packet In which is not ARP!")
+            self.logger.info("%s: Got Packet In: %s", datetime.datetime.now().strftime('%H:%M:%S.%f'),
+                             etherFrame.ethertype)
+            return
+
+    # Sends an ARP response to the contacting host with the
+    # real MAC address of a server.
+    def arp_response(self, datapath, packet, etherFrame, ofp_parser, ofp, in_port):
+        arpPacket = packet.get_protocol(arp.arp)
+        dstIp = arpPacket.src_ip
+        srcIp = arpPacket.dst_ip
+        dstMac = etherFrame.src
+
+        # If the ARP request isn't from one of the two servers,
+        # choose the target/source MAC address from one of the servers;
+        # else the target MAC address is set to the one corresponding
+        # to the target host's IP.
+        if dstIp != self.H5_ip and dstIp != self.H6_ip:
+            srcMac = self.ip_to_mac[self.current_server]
+            self.logger.info("%s: Sending ARP reply to HOST", datetime.datetime.now().strftime('%H:%M:%S.%f'))
+        else:
+            srcMac = self.ip_to_mac[srcIp]
+            self.logger.info("%s: Sending ARP reply to SERVER", datetime.datetime.now().strftime('%H:%M:%S.%f'))
+
+        e = ethernet.ethernet(dstMac, srcMac, ether_types.ETH_TYPE_ARP)
+        a = arp.arp(1, 0x0800, 6, 4, 2, srcMac, srcIp, dstMac, dstIp)
+        p = Packet()
+        p.add_protocol(e)
+        p.add_protocol(a)
+        p.serialize()
+
+        # ARP action list
+        actions = [ofp_parser.OFPActionOutput(ofp.OFPP_IN_PORT)]
+        # ARP output message
+        out = ofp_parser.OFPPacketOut(
+            datapath=datapath,
+            buffer_id=ofp.OFP_NO_BUFFER,
+            in_port=in_port,
+            actions=actions,
+            data=p.data
+        )
+        datapath.send_msg(out)  # Send out ARP reply
+        self.logger.info("%s: ARP reply send", datetime.datetime.now().strftime('%H:%M:%S.%f'))
+
+    # Sets up the flow table in the switch to map IP addresses correctly.
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match,
+                                    instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+    def send_group_mod(self, datapath):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # Hardcoding the stuff, as we already know the topology diagram.
+        # Group table1
+        # Receiver port3 (host connected), forward it to port1(switch) and Port2(switch)
+        LB_WEIGHT1 = 30  # percentage
+        LB_WEIGHT2 = 70  # percentage
+
+        watch_port = ofproto_v1_3.OFPP_ANY
+        watch_group = ofproto_v1_3.OFPQ_ALL
+
+        actions1 = [ofp_parser.OFPActionSetField(ipv4_dst=self.H5_ip),
+                    ofp_parser.OFPActionSetField(eth_dst=self.H5_mac),
+                    parser.OFPActionOutput(self.ip_to_port(self.H5_ip))]
+        actions2 = [ofp_parser.OFPActionSetField(ipv4_dst=self.H6_ip),
+                    ofp_parser.OFPActionSetField(eth_dst=self.H6_mac),
+                    parser.OFPActionOutput(self.ip_to_port(self.H6_ip))]
+
+        buckets = [parser.OFPBucket(LB_WEIGHT1, watch_port, watch_group, actions=actions1),
+                   parser.OFPBucket(LB_WEIGHT2, watch_port, watch_group, actions=actions2)]
+
+        req = parser.OFPGroupMod(datapath, ofproto.OFPGC_ADD,
+                                 ofproto.OFPGT_SELECT, self.group_table_id, buckets)
+        datapath.send_msg(req)
+
