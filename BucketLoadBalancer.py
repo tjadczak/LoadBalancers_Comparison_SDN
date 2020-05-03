a4ccd7615dee6a68b87a321926acc38036f948f6
jadczak <tomasz.jadczak1996@gmail.com>
2020-05-03 17:59:00 +0200

    Buckets

--- BucketLoadBalancer.py
+++ BucketLoadBalancer.py
@@ -95,10 +95,38 @@ class SimpleLoadBalancer(app_manager.RyuApp):
         super(SimpleLoadBalancer, self).__init__(*args, **kwargs)
         self.datapaths = {}
         self.current_server = self.H5_ip
-        self.monitor_thread = hub.spawn(ElephantFlowMonitor())
+        self.monitor_thread = hub.spawn(self.ElephantFlowMonitor())
         self.logger.info("--------------------------------------------------------------")
         self.logger.info("%s: STARTUP", datetime.datetime.now().strftime('%H:%M:%S.%f'))
         self.logger.info("--------------------------------------------------------------")
+
+    def ElephantFlowMonitor(self):
+        rt = 'http://127.0.0.1:8008'
+
+        flowUdp = {'keys': 'link:inputifindex,ipsource,ipdestination,ipprotocol,udpsourceport,udpdestinationport',
+                   'value': 'bytes'}
+        # flowTcp = {'keys':'link:inputifindex,ipsource,ipdestination,ipprotocol,tcpsourceport,tcpdestinationport','value':'bytes'}
+        requests.put(rt + '/flow/pair/json', data=json.dumps(flowUdp))
+        # requests.put(rt+'/flow/pair/json',data=json.dumps(flowTcp))
+
+        threshold = {'metric': 'pair', 'value': 1, 'byFlow': True, 'timeout': 1}
+        requests.put(rt + '/threshold/elephant/json', data=json.dumps(threshold))
+
+        eventurl = rt + '/events/json?thresholdID=elephant&maxEvents=10&timeout=60'
+        eventID = -1
+        while True:
+            r = requests.get(eventurl + "&eventID=" + str(eventID))
+            if r.status_code != 200: break
+            events = r.json()
+            if len(events) == 0:
+                # print("Length of events = 0, continuing")
+                continue
+
+            eventID = events[0]["eventID"]
+            events.reverse()
+            for e in events:
+                print("{}: Elephant flow detected {}".format(datetime.datetime.now().strftime('%H:%M:%S.%f'), e['flowKey']))
+            hub.sleep(1)
 
     @set_ev_cls(ofp_event.EventOFPStateChange, [MAIN_DISPATCHER, DEAD_DISPATCHER])
     def _state_change_handler(self, ev):
