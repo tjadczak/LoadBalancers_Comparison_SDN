0656e5f73f9605ec4e9b93ac68ea73d6e040c74c
jadczak <tomasz.jadczak1996@gmail.com>
2020-05-03 17:41:06 +0200

    Buckets

--- BucketLoadBalancer.py
+++ BucketLoadBalancer.py
@@ -13,6 +13,36 @@ from ryu.lib.packet import arp
 from operator import attrgetter
 from ryu.lib import hub
 import datetime
+import requests
+import json
+
+
+def sendElephantFlowMonitor():
+    rt = 'http://127.0.0.1:8008'
+
+    flowUdp = {'keys': 'link:inputifindex,ipsource,ipdestination,ipprotocol,udpsourceport,udpdestinationport',
+               'value': 'bytes'}
+    # flowTcp = {'keys':'link:inputifindex,ipsource,ipdestination,ipprotocol,tcpsourceport,tcpdestinationport','value':'bytes'}
+    requests.put(rt + '/flow/pair/json', data=json.dumps(flowUdp))
+    # requests.put(rt+'/flow/pair/json',data=json.dumps(flowTcp))
+
+    threshold = {'metric': 'pair', 'value': 1, 'byFlow': True, 'timeout': 1}
+    requests.put(rt + '/threshold/elephant/json', data=json.dumps(threshold))
+
+    eventurl = rt + '/events/json?thresholdID=elephant&maxEvents=10&timeout=60'
+    eventID = -1
+    while 1 == 1:
+        r = requests.get(eventurl + "&eventID=" + str(eventID))
+        if r.status_code != 200: break
+        events = r.json()
+        if len(events) == 0:
+            # print("Length of events = 0, continuing")
+            continue
+
+        eventID = events[0]["eventID"]
+        events.reverse()
+        for e in events:
+            print("%s: Elephant flow detected %s", datetime.datetime.now().strftime('%H:%M:%S.%f'), e['flowKey'])
 
 """ 
     TOPOLOGY:
@@ -65,6 +95,7 @@ class SimpleLoadBalancer(app_manager.RyuApp):
         super(SimpleLoadBalancer, self).__init__(*args, **kwargs)
         self.datapaths = {}
         self.current_server = self.H5_ip
+        sendElephantFlowMonitor()
         self.logger.info("--------------------------------------------------------------")
         self.logger.info("%s: STARTUP", datetime.datetime.now().strftime('%H:%M:%S.%f'))
         self.logger.info("--------------------------------------------------------------")
