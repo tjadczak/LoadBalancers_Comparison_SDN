28d77ef3d89d05030768de5e16f51938b981b8db
jadczak <tomasz.jadczak1996@gmail.com>
2020-05-03 18:56:19 +0200

    Buckets

--- BucketLoadBalancer.py
+++ BucketLoadBalancer.py
@@ -17,33 +17,6 @@ import requests
 import json
 
 
-def ElephantFlowMonitor():
-    rt = 'http://127.0.0.1:8008'
-
-    flowUdp = {'keys': 'link:inputifindex,ipsource,ipdestination,ipprotocol,udpsourceport,udpdestinationport',
-               'value': 'bytes'}
-    # flowTcp = {'keys':'link:inputifindex,ipsource,ipdestination,ipprotocol,tcpsourceport,tcpdestinationport','value':'bytes'}
-    requests.put(rt + '/flow/pair/json', data=json.dumps(flowUdp))
-    # requests.put(rt+'/flow/pair/json',data=json.dumps(flowTcp))
-
-    threshold = {'metric': 'pair', 'value': 1, 'byFlow': True, 'timeout': 1}
-    requests.put(rt + '/threshold/elephant/json', data=json.dumps(threshold))
-
-    eventurl = rt + '/events/json?thresholdID=elephant&maxEvents=10&timeout=60'
-    eventID = -1
-    while 1 == 1:
-        r = requests.get(eventurl + "&eventID=" + str(eventID))
-        if r.status_code != 200: break
-        events = r.json()
-        if len(events) == 0:
-            # print("Length of events = 0, continuing")
-            continue
-
-        eventID = events[0]["eventID"]
-        events.reverse()
-        for e in events:
-            print("{}: Elephant flow detected {}".format(datetime.datetime.now().strftime('%H:%M:%S.%f'), e['flowKey']))
-
 """ 
     TOPOLOGY:
             h1 --- *            * --- h5 (server)
@@ -115,7 +88,7 @@ class SimpleLoadBalancer(app_manager.RyuApp):
         eventurl = rt + '/events/json?thresholdID=elephant&maxEvents=10&timeout=60'
         eventID = -1
         while True:
-            r = requests.get(eventurl + "&eventID=" + str(eventID))
+            r = requests.get(eventurl + "&eventID=" + str(eventID), timeout=0.001)
             if r.status_code != 200: break
             events = r.json()
             if len(events) == 0:
